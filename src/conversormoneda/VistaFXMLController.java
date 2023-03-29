799ac52e793b2900a21a796166713140da97fe12
Francisco-Back <fbacks@miumg.edu.gt>
2023-03-28 21:55:58 -0600

    MonedaCombox

--- src/conversormoneda/VistaFXMLController.java
+++ src/conversormoneda/VistaFXMLController.java
@@ -4,6 +4,7 @@
  */
 package conversormoneda;
 
+import Model.Moneda;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -23,7 +24,7 @@ import javax.swing.JOptionPane;
 public class VistaFXMLController implements Initializable {
 
     @FXML
-    private ComboBox<?> CboxDivisa;
+    private ComboBox<Moneda> CboxDivisa;
     @FXML
     private Label LabelMone;
     @FXML
@@ -36,79 +37,84 @@ public class VistaFXMLController implements Initializable {
     private ToggleButton BtnTonggle;
     @FXML
     private Label LabelNotifi;
-    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-     LabelNotifi.setText("Online");
-    }    
-    
-   
+        LabelNotifi.setText("Online");
 
+    }
+  
+
+    //Metodo para recibe el text area y los covierte en enteros
     @FXML
     private void ConvertirModenaactual(ActionEvent event) {
+
         try {
-        
-                 if(TextAreaone.getText()=="" && TextAreatwo.getText()==""){
-        JOptionPane.showMessageDialog(null, "Campos Vacios",  "Alerta!", 0);
-        }else{ 
-            
-          if(TextAreaone.getText().isEmpty()==false && TextAreatwo.getText().isEmpty()==false){
-              JOptionPane.showMessageDialog(null, "No se puede ingresar\n"+"Los dos datos",  "Alerta!", 0);  
-          }else{
-              
-               int Modena1=Integer.parseInt( TextAreaone.getText());
-             int Modena2=Integer.parseInt(TextAreatwo.getText());
-        if(Modena1!=-1){
-            System.out.println("Modena 1"+Modena1);
-            
-        }if(Modena2!=-1){
-                System.out.println("Modena 2"+Modena2);
-                 JOptionPane.showMessageDialog(null, "Dato incorrecto\n"+"Solo Enteros",  "Alerta!", 0); 
-         }}
-            
-       
-    }
-        } catch (Exception e) {
+
+            if ("".equals(TextAreaone.getText()) && "".equals(TextAreatwo.getText())) {
+                JOptionPane.showMessageDialog(null, "Campos Vacios", "Alerta!", 0);
+            } else {
+
+                if (TextAreaone.getText().isEmpty() == false && TextAreatwo.getText().isEmpty() == false) {
+                    JOptionPane.showMessageDialog(null, """
+                                                  No se puede ingresar
+                                                  Los dos datos""", "Alerta!", 0);
+                } else {
+                    if (TextAreaone.getText().isEmpty() == false) {
+                        int Modena1 = Integer.parseInt(TextAreaone.getText());
+
+                        System.out.println("Modena 1" + Modena1);
+
+                    }
+                    if (TextAreatwo.getText().isEmpty() == false) {
+                        int Modena2 = Integer.parseInt(TextAreatwo.getText());
+                        System.out.println("Modena 2" + Modena2);
+
+                    }
+
+                }
+            }
+        } catch (NumberFormatException e) {
             System.out.println(e);
-            
+            JOptionPane.showMessageDialog(null, """
+                                                 Dato incorrecto
+                                                 Solo Enteros""", "Alerta!", 0);
+
         }
- 
-       
-    
+
         TextAreaone.setText("");
         TextAreatwo.setText("");
-        
+
     }
 
     @FXML
     private void SalirPrograma(ActionEvent event) {
-        int resp = JOptionPane.showConfirmDialog(null, "Desea salir del programa\n"+"¿Esta seguro?",//<- EL MENSAJE
-            "Alerta!"/*<- El título de la ventana*/, JOptionPane.YES_NO_OPTION/*Las opciones (si o no)*/, JOptionPane.WARNING_MESSAGE/*El tipo de ventana, en este caso WARNING*/);
-           
-       if(resp==0){
-               JOptionPane.showMessageDialog(null, "Gracias por Utilizar Nuestra App");
-            
-        System.exit(0);
+        int resp = JOptionPane.showConfirmDialog(null, "Desea salir del programa\n" + "¿Esta seguro?",//<- EL MENSAJE
+                "Alerta!"/*<- El título de la ventana*/, JOptionPane.YES_NO_OPTION/*Las opciones (si o no)*/, JOptionPane.WARNING_MESSAGE/*El tipo de ventana, en este caso WARNING*/);
+
+        if (resp == 0) {
+            JOptionPane.showMessageDialog(null, "Gracias por Utilizar Nuestra App");
+
+            System.exit(0);
         }
     }
 
     @FXML
     private void TonggleOffline(ActionEvent event) {
-       
-         if(BtnTonggle.isSelected()==true){
-             System.out.println( BtnTonggle.isSelected());
-                LabelNotifi.setText("Offline");
-          
-               
-         }else{
-                  System.out.println( BtnTonggle.isSelected());
-               LabelNotifi.setText("Oline");
-               
-         }
+
+        if (BtnTonggle.isSelected() == true) {
+            System.out.println(BtnTonggle.isSelected());
+            LabelNotifi.setText("Offline");
+           CboxDivisa.getItems().addAll(new Moneda());
+
+        } else {
+            System.out.println(BtnTonggle.isSelected());
+            LabelNotifi.setText("Oline");
+
+        }
     }
-    
+
 }
